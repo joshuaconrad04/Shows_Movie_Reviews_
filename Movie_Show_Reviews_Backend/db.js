ade8fcc2b3997ca971d97ee393bac72f215d8443
joshuaconrad04 <joshua.conrad@internoptima.com>
2025-01-01 13:23:29 -0600

    docker files added

--- Movie_Show_Reviews_Backend/db.js
+++ Movie_Show_Reviews_Backend/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 
 const db = await mysql.createConnection({
-  host: 'localhost', // Replace with your DB host
+  host: 'mysql', // Replace with your DB host
   user: 'root',      // Replace with your MySQL username
   password: 'Boomboom#1212', // Replace with your MySQL password
   database: 'Shows_Movie_Reviews_', // Replace with your database name
